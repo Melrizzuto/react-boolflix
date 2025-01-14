e3119441f416eca7b2edba1117d8fbfb6edb22d0
Melrizzuto <melrizzuto4@gmail.com>
2025-01-14 18:12:37 +0100

    added style to card overlay

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,6 +12,11 @@ const Card = ({ item }) => {
                 <h3>{item.title || item.name}</h3>
                 <p>{item.overview}</p>
                 {item.media_type === 'tv' && <span className={styles.tvBadge}>TV Show</span>}
+
+                {/* Nuove informazioni da visualizzare */}
+                <p><strong>Titolo Originale:</strong> {item.original_title || item.original_name}</p>
+                <p><strong>Lingua:</strong> {item.original_language}</p>
+                <p><strong>Voto:</strong> {item.vote_average}</p>
             </div>
         </div>
     );
